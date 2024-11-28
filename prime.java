a6ee97a7ca567b2aefc3fbcb6597cc823d4687e9
yashaswi-03 <yashaswiboga@gmail.com>
2024-11-28 10:46:21 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,25 @@
+class prime {
+    static boolean isPrime(int n)
+    {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+
+    public static void main(String args[])
+    {
+        if (isPrime(11))
+            System.out.println(" true");
+        else
+            System.out.println(" false");
+        if (isPrime(15))
+            System.out.println(" true");
+        else
+            System.out.println(" false");
+    }
+}
